b3d46c4aaca13316e47448d59436539112222ab4
yeuda hutzler <yh0583275101@gmail.com>
2026-06-21 00:09:55 +0300

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -92,7 +92,7 @@ def chat():
             if os.path.exists(local_audio_path): os.remove(local_audio_path)
             if os.path.exists(tts_filename): os.remove(tts_filename)
 
-            return f"id_list_message=f-answer_{phone}&read=f-greeting=user_audio,,record"
+            return f"id_list_message=f-answer_{phone}&go_to_folder=?next_step=2"
 
         # כניסה ראשונית - משמיע את קובץ ה-greeting שהעלת ומקליט
         return f"read=f-greeting=user_audio,,record"
