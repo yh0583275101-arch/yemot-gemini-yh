8f94d8a0f1c29586804367c62aa2fbc49dbcf3d5
yeuda hutzler <yh0583275101@gmail.com>
2026-06-22 21:35:47 +0300

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -94,7 +94,7 @@ def chat():
             if os.path.exists(tts_filename): os.remove(tts_filename)
 
             # הפקודה המדויקת שמשמיעה מתוך שלוחה 1 ומבקשת להקליט שוב
-            return f"answer_{phone}"
+            return f"play=answer_{phone}"
 
         # כניסה ראשונית לשלוחה
         return f"read=f-greeting=user_audio,,record"
