3e6d11eb2c31736f660c70a2b3980eec96f451ca
yeuda hutzler <yh0583275101@gmail.com>
2026-06-26 18:42:28 +0300

    Update chat.py

--- modules/chat.py
+++ modules/chat.py
@@ -139,7 +139,7 @@ def chat():
             if os.path.exists(local_audio_path): os.remove(local_audio_path)
             if os.path.exists(tts_filename): os.remove(tts_filename)
 
-            return f"read=f-/tmp/{phone}"
+            return f"read=f-{phone}=user_audio,,record,,,,no"
 
 
         # כניסה ראשונית לשלוחה
